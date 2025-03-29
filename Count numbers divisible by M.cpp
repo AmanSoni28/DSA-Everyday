8f328dba69a3e18bc3db18fb8b198666147233fe
Aman Soni <amansoni0228@gmail.com>
2025-03-29 20:32:19 +0530

    Learn

---
+++ Count numbers divisible by M.cpp
@@ -0,0 +1,16 @@
+// topic link : https://www.geeksforgeeks.org/problems/count-numbers-divisible-by-m1524/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+class Solution {
+    public:
+      int countDivisibles(int A, int B, int M) {
+          int count=0;
+      for(int i=A;i<=B;i++)
+      {
+          if(i%M==0)
+          count++;
+          else
+          continue;
+      }
+      return count;
+      }
+  };

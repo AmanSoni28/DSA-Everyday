b16097923fb25ca8e558cf79c6c1c105e55afe46
Aman Soni <amansoni0228@gmail.com>
2025-03-29 23:10:55 +0530

    Learn

---
+++ 4.Third Maximum Number.cpp
@@ -0,0 +1,25 @@
+/* topic link: https://leetcode.com/problems/third-maximum-number/
+
+class Solution {
+    public:
+        int thirdMax(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+         set<int>s;
+         for(int i : nums)
+         {
+            s.insert(i);
+         }
+           vector<int>vec;
+         for(int j: s)
+         {
+            vec.push_back(j);
+         }
+         int n=vec.size();
+         if(n>=3)
+         return vec[n-3];
+         else if(n==2)
+         return vec[1];
+         else 
+         return vec[0];
+        }
+    };                         */

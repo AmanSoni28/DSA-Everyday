d1c2f11876a7cc1f24c53bdcb084f910bde915f8
Aman Soni <amansoni0228@gmail.com>
2025-03-30 00:51:37 +0530

    Learn

---
+++ 3.LCM And GCD.cpp
@@ -0,0 +1,22 @@
+//    topic link : https://www.geeksforgeeks.org/problems/lcm-and-gcd4516/1?page=1
+
+  class Solution {
+    public:
+      vector<int> lcmAndGcd(int a, int b) {
+          vector<int>vec;
+          int n=min(a,b),c;
+          for(int i=1;i<=n;i++)
+          {
+              if(a%i==0 && b%i==0)
+              c=i;
+          }
+          
+           int LCM = (a * b)/c;               //LCM(a, b) * GCD(a, b) = a * b
+           
+          vec.push_back(LCM);
+          vec.push_back(c);
+         
+          return vec;
+          
+      }
+  };                                           

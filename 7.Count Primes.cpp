3edcd4a1af20eea6e65c9bc501eb63b56b940c2f
Aman Soni <amansoni0228@gmail.com>
2025-04-05 22:15:50 +0530

    IMP

---
+++ 7.Count Primes.cpp
@@ -0,0 +1,22 @@
+// topic link : https://leetcode.com/problems/count-primes/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// T(c)=>n*log(lon(n)),S(c)=>n
+class Solution {
+    public:
+        int countPrimes(int n) {
+            int count=0;
+            vector<int>vec(n,true);
+            for(int i=2;i<n;i++){
+                if(vec[i]==true){
+                    count++;
+                    for(int j=i;j<n;j=j+i){
+                        vec[j]=false;
+                    }
+                }
+            }
+            return count;
+        }
+    };
